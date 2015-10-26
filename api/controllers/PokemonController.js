f8503224b425192602175c9da9f78fe2114bd2fd
Alexis Zambrano <alexis2tj@hotmail.com>
2015-10-26 14:50:58 -0500

    anadir hola y adios en controlador pokemon

---
+++ api/controllers/PokemonController.js
@@ -0,0 +1,17 @@
+/**
+ * PokemonController
+ *
+ * @description :: Server-side logic for managing pokemons
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	hola: function (req, res) {
+	 return res.send("hola desde el controlador pokemon");
+ },
+
+ adios: function (req, res) {
+	  retur res.send("Adios desde el controlador");
+ }
+
+};

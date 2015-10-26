d0d4e3134ba9caffd8e1a571105a1be90d1b57d3
Alexis Zambrano <alexis2tj@hotmail.com>
2015-10-26 14:05:37 -0500

    creacion de la tabla de pokemon

---
+++ api/models/Pokemon.js
@@ -0,0 +1,36 @@
+/**
+* Pokemon.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    nombre:{
+      type: 'string',
+      unique: true,
+      required: true
+
+    },
+    tipo:{
+      type: 'string'
+      enum: ['agua', 'fuego','tierra','psiquico','hierba','electrico']
+    },
+    habilidadEspecial: {
+      type: 'string',
+      defaultsTo: 'Ninguno'
+    },
+    numerPokemon:{
+      type: 'integer',
+      required: true
+    },
+    fechaVista:{
+      type:'date'
+    }
+
+
+
+  }
+};
